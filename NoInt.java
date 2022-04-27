99372ee8f0bbcec3e6f8527ee666f8bba3cdde07
Francisco Mateus <fmfr@outlook.com>
2022-04-27 13:53:01 -0300

    filas simples

---
+++ NoInt.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fila;
+
+/**
+ *
+ * @author fmfr
+ */
+public class NoInt {
+    public int valor;
+    public NoInt proximo;
+
+    public NoInt(int valor) {
+        this.valor = valor;
+        proximo = null;
+    }
+
+    @Override
+    public String toString() {
+        return "No{" + "valor=" + valor + '}';
+    }
+
+}
